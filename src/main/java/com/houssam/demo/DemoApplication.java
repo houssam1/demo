6e63794420188d45e829c59095739a4e87728311
Houssam Tliouant <tliouant.houssam@gmail.com>
2020-03-24 19:00:31 +0100

    [master] add pom conf to deploy a war project

--- src/main/java/com/houssam/demo/DemoApplication.java
+++ src/main/java/com/houssam/demo/DemoApplication.java
@@ -2,13 +2,14 @@ package com.houssam.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -16,7 +17,7 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "hello world!";
+		return "hell world!";
 	}
 
 	@GetMapping("/hello/{name}")
